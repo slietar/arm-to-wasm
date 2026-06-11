02cf78a6def03d672342968aa312e7cee7388b46
Simon Liétar <simon@lietar.net>
2026-06-11 23:16:39 +0200

    Add runtime

---
+++ runtime/src/main.rs
@@ -0,0 +1,48 @@
+use wasmtime::*;
+
+fn main() -> wasmtime::Result<()> {
+    let engine = Engine::default();
+
+    let bytes = std::fs::read("../reboot/output.wasm")?;
+    let module = Module::new(&engine, bytes)?;
+
+    let mut linker = Linker::new(&engine);
+
+    linker.func_wrap("env", "syscall_handler", |mut caller: Caller<'_, ()>, param: i32, x8: i64, x0: i64, x1: i64, x2: i64, x3: i64, x4: i64, x5: i64| -> i64 {
+        match x8 {
+            0x40 => {
+                let fd = x0;
+                let ptr = x1 as u32;
+                let len = x2 as u32;
+
+                let memory = caller.get_export("mem").unwrap().into_memory().unwrap();
+
+                let data = memory.data(&caller);
+                let slice = &data[ptr as usize..(ptr + len) as usize];
+
+                eprintln!("Writing to fd {}: {:?}", fd, std::str::from_utf8(slice).unwrap());
+            },
+            0x5d => {
+                eprintln!("Exit called with code: {}", x0);
+            },
+            _ => {
+                eprint!("syscall_handler called with param: {}, x8: {}, x0: {}, x1: {}, x2: {}, x3: {}, x4: {}, x5: {}\n", param, x8, x0, x1, x2, x3, x4, x5);
+            }
+        }
+
+        0
+    })?;
+
+    let mut store: Store<_> = Store::new(&engine, ());
+
+    // Instantiation of a module requires specifying its imports and then
+    // afterwards we can fetch exports by name, as well as asserting the
+    // type signature of the function with `get_typed_func`.
+    let instance = linker.instantiate(&mut store, &module)?;
+    let hello = instance.get_typed_func::<(), ()>(&mut store, "_entry")?;
+
+    // And finally we can call the wasm!
+    hello.call(&mut store, ())?;
+
+    Ok(())
+}
